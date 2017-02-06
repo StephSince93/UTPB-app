b99d44a2de7dfd7d0b01d4d4ab896f5824c8a27a
Stephen Romero <stephen.romero93@gmail.com>
2017-02-06 14:41:48 -0600

    added the hyperlinks to the canvas and mytupb sites when the buttons are pressed

--- UTPB app/ViewController.swift
+++ UTPB app/ViewController.swift
@@ -11,26 +11,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var canvasButton: UIButton!
     
- //   @IBOutlet weak var canvasButton: UIButton!
-
+    @IBOutlet weak var myUTPBButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-   //     locationButton.clipsToBounds = true
+        //     locationButton.clipsToBounds = true
         locationButton.layer.cornerRadius = 10
         locationButton.layer.borderWidth = 3.0
         locationButton.layer.borderColor = (UIColor(red: 1.0, green: 0.5, blue: 0, alpha: 1.0)).cgColor
- //       canvasButton.layer.cornerRadius = 10
-  //      canvasButton.layer.borderWidth = 3.0
-   //     canvasButton.layer.borderColor = (UIColor(red: 1.0, green: 0.5, blue: 0, alpha: 1.0)).cgColor
-       
+        canvasButton.layer.cornerRadius = 10
+        canvasButton.layer.borderWidth = 3.0
+        canvasButton.layer.borderColor = (UIColor(red: 1.0, green: 0.5, blue: 0, alpha: 1.0)).cgColor
+        myUTPBButton.layer.cornerRadius = 10
+        myUTPBButton.layer.borderWidth = 3.0
+        myUTPBButton.layer.borderColor = (UIColor(red: 1.0, green: 0.5, blue: 0, alpha: 1.0)).cgColor
+        //   canvasButton.addTarget(self, action: "canvasTapped", for: .touchUpInside)
         
     }
-
+    
     @IBOutlet weak var locationButton: UIButton!
     
     
-  
+    //Canvas Button action!
+    @IBAction func canvasTapped(_ sender: Any) {
+        //links to the canvas website
+        if let url = URL(string: "https://utpb.instructure.com")
+        {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        
+    }
+    //myUPTB Button action!
+    @IBAction func myUTPBTapped(_ sender: Any) {
+        //links to the myUPTB website
+        if let url = URL(string: "http://my.utpb.edu")
+        {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    
 }
-
